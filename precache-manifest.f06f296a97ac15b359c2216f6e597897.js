1ac6c71ff8fb1fd842665093ccb09a56e799b8b2
ahmedbouzana <ah.bouzana@gmail.com>
2019-01-29 15:46:29 +0100

    Updates

---
+++ precache-manifest.f06f296a97ac15b359c2216f6e597897.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e150e064fc9a058e21d2",
+    "url": "/router-app/static/js/runtime~main.e150e064.js"
+  },
+  {
+    "revision": "cd74960e5480865a4563",
+    "url": "/router-app/static/js/main.cd74960e.chunk.js"
+  },
+  {
+    "revision": "adda6f4ded957a11decf",
+    "url": "/router-app/static/js/1.adda6f4d.chunk.js"
+  },
+  {
+    "revision": "cd74960e5480865a4563",
+    "url": "/router-app/static/css/main.f41464d2.chunk.css"
+  },
+  {
+    "revision": "43f9733ac59f01c07d30cb0641b311fa",
+    "url": "/router-app/index.html"
+  }
+];
